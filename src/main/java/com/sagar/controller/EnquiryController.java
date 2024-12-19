7f2e2293471b0dedf58e82338c018f76120890d6
sagarkumar-cloud <pattanayaksagarkumar@gmail.com>
2024-12-20 01:19:39 +0530

    add enquiry

--- src/main/java/com/sagar/controller/EnquiryController.java
+++ src/main/java/com/sagar/controller/EnquiryController.java
@@ -1,7 +1,13 @@
 package com.sagar.controller;
 
+import com.sagar.entity.EnquiryStudent;
 import com.sagar.service.EnquiryService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class EnquiryController {
@@ -12,5 +18,25 @@ public class EnquiryController {
         this.enquiryService = enquiryService;
     }
 
+    @GetMapping("/enquiry")
+      public String addEnquiry(Model model){
+          EnquiryStudent enquiryObj = new EnquiryStudent();
+          model.addAttribute("enquiry",enquiryObj);
+        return"enquiry_form";
+      }
 
+      @PostMapping("/addEnq")
+      public String handleAddEnquiry(Model model, EnquiryStudent enquiry, HttpServletRequest request) throws Exception {
+          HttpSession session = request.getSession(false);
+          Integer counsellorId=(Integer) session.getAttribute("counsellorId");
+
+          boolean isSaved = enquiryService.addEnquiry(enquiry, counsellorId);
+          if(isSaved){
+              model.addAttribute("sMsg","enquiry added..");
+          }else{
+              model.addAttribute("eMsg","enquiry adding failed..!!");
+          }
+
+          return "enquiry_form";
+      }
 }
